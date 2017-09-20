649f05ac8aa2e9d493d71eaa6775597cd195ce3a
CheeLoong <cheeloonglian@gmail.com>
2017-09-21 01:39:08 +0800

    Modified name for Simulation - Simulating a Linear Model.R

--- R Programming Week 4/Simulation - Simulating a Linear Model.R
+++ R Programming Week 4/Simulation - Simulating a Linear Model.R
@@ -19,7 +19,7 @@ plot(x,y) # shows x variable is binary and y variable is continuous
 
 ## Generating Random Numbers From a Generalized Linear Model
 ## Suppose we want to simulate from a Poisson model where Y ~ Poisson(u)
-## log u = B_0 + B_1*x, B_0 = 0.5, B_1 = 0.3
+# log u = B_0 + B_1*x, B_0 = 0.5, B_1 = 0.3
 set.seed(1)
 x <- rnorm(100)
 log.mu <- 0.5 + 0.3 * x #generate linear predictor log of mu
