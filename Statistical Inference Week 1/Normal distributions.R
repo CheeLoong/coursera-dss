1650905bac22b83cb8b139c0b8ef606ac9d9b4d8
CheeLoong <cheeloonglian@gmail.com>
2017-10-17 16:01:52 +0800

    Added Normal distributions.R

---
+++ Statistical Inference Week 1/Normal distributions.R
@@ -0,0 +1,13 @@
+# Normal distributions
+
+## Probability Calculation
+
+pnorm(1160, mean = 1020, sd = 50, lower.tail = FALSE)
+
+# Alternatively,
+
+pnorm(2.8, lower.tail = FALSE)
+
+## Quantile Calculation
+
+qnorm(0.75, mean = 1020, sd = 50)
