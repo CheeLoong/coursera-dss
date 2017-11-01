fc8f8a7064e997f04b0088129b6aaf820de03bf7
CheeLoong <cheeloonglian@gmail.com>
2017-11-01 22:26:26 +0800

    Added Linear Least Squares Coding Example.R

---
+++ Regression Modelling Week 1/Linear Least Squares Coding Example.R
@@ -0,0 +1,46 @@
+# Linear Least Squares Coding Example
+
+## Revisiting Galton's data
+### Double check our calculations using R
+y <- galton$child
+x <- galton$parent
+beta1 <- cor(y, x) *  sd(y) / sd(x)
+beta0 <- mean(y) - beta1 * mean(x)
+rbind(c(beta0, beta1), coef(lm(y ~ x))) # manual calculation and lm calculation is exactly the same
+
+## Revisiting Galton's data
+### Reversing the outcome/predictor relationship
+y <- galton$child
+x <- galton$parent
+beta1 <- cor(y, x) *  sd(x) / sd(y)
+beta0 <- mean(x) - beta1 * mean(y)
+rbind(c(beta0, beta1), coef(lm(x ~ y)))
+
+## Revisiting Galton's data
+### Regression through the origin yields an equivalent slope if you center the data first
+yc <- y - mean(y)
+xc <- x - mean(x)
+beta1 <- sum(yc * xc) / sum(xc ^ 2)
+c(beta1, coef(lm(y ~ x))[2])
+lm(yc ~ xc - 1) # regression through origin after centering the data
+
+## Revisiting Galton's data
+### Normalizing variables results in the slope being the correlation
+yn <- (y - mean(y))/sd(y)
+xn <- (x - mean(x))/sd(x)
+c(cor(y, x), cor(yn, xn), coef(lm(yn ~ xn))[2])
+
+## Revisiting Galton's data
+### Fitting a linear regression line in the data
+library(dplyr)
+freqData <- as.data.frame(table(galton$child, galton$parent))
+names(freqData) <- c("child", "parent", "freq")
+freqData$child <- as.numeric(as.character(freqData$child))
+freqData$parent <- as.numeric(as.character(freqData$parent))
+g <- ggplot(filter(freqData, freq > 0), aes(x = parent, y = child))
+g <- g  + scale_size(range = c(2, 20), guide = "none" )
+g <- g + geom_point(colour="grey50", aes(size = freq+20, show_guide = FALSE))
+g <- g + geom_point(aes(colour=freq, size = freq))
+g <- g + scale_colour_gradient(low = "lightblue", high="white") 
+g <- g + geom_smooth(method = "lm", formula = y ~ x)
+g
